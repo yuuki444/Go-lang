0943a89fb7211d61dabff3ec863a99cbc80e558f
yuuki <academymansur123@gmail.com>
2026-09-04 20:22:25 +0500

    tcl server fix

--- 1/server/main.go
+++ 1/server/main.go
@@ -1,53 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"strings"
 	"time"
 )
 
 func main() {
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		fmt.Println("start error :", err)
+		fmt.Println("start error:", err)
 		return
 	}
 	defer listener.Close()
 
 	conn, err := listener.Accept()
 	if err != nil {
-		fmt.Println("connect error:", err)
+		fmt.Println("connection error:", err)
 		return
 	}
 	defer conn.Close()
 
-	fmt.Println("client:", conn.RemoteAddr().String())
+	fmt.Println("Клиент:", conn.RemoteAddr().String())
 
-	reader := bufio.NewReader(conn)
-	message, err := reader.ReadString('\n')
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("read error:", err)
+		fmt.Println("read error :", err)
 		return
 	}
 
-	message = strings.TrimSpace(message)
+	message := string(buf[:n])
 	fmt.Println("Получено:", message)
 
-var response string
-
-if message == "/help" {
-    response = "Commands: /help, /time\n"
-} else if message == "/time" {
-    response = time.Now().Format("15:04:05") + "\n"
-} else {
-    response = "Echo: " + message + "\n"
-}
+	var response string
+	if message == "/help\n" || message == "/help" {
+		response = "Commands: /help, /time\n"
+	} else if message == "/time\n" || message == "/time" {
+		response = time.Now().Format("15:04:05") + "\n"
+	} else {
+		response = "Echo: " + message
+	}
 
 	_, err = conn.Write([]byte(response))
 	if err != nil {
-		fmt.Println("response error ", err)
+		fmt.Println("response error :", err)
 		return
 	}
 }
